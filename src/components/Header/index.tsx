28a680fffdfe04edf9d97c2b7129819adde22371
joaogabriels <joaogabrielsevangelista@gmail.com>
2022-03-28 10:57:59 -0300

    Header alterations

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,6 +10,9 @@ const Header = (): JSX.Element => {
   // const { cart } = useCart();
   // const cartSize = // TODO;
 
+  const { cart } = useCart();
+  const cartSize = cart.length;
+
   return (
     <Container>
       <Link to="/">
@@ -20,7 +23,9 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */
+              cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`
+            }
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
